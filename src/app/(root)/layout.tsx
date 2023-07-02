ed3702eaaa3309eb98d052b24c0de2c67685147e
Milan Patel <patelmilanun@gmail.com>
2023-07-02 12:16:46 +0530

    feat: Add create store dialog globally

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import { redirect } from 'next/navigation';
+import { auth } from '@clerk/nextjs';
+
+export default async function SetupLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect('/sign-in');
+  }
+
+  return <>{children}</>;
+}
