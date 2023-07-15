650716b73d43185ecd65c0a35ad7400175c58b98
Milan Patel <patelmilanun@gmail.com>
2023-07-15 22:12:55 +0530

    feat: Add size module - Add CRUD apis and UI pages for size

--- src/app/(dashboard)/[storeId]/sizes/components/columns.tsx
+++ src/app/(dashboard)/[storeId]/sizes/components/columns.tsx
@@ -4,22 +4,21 @@ import { ColumnDef } from '@tanstack/react-table';
 
 import CellAction from './cell-action';
 
-export type CategoryColumn = {
+export type SizeColumn = {
   id: string;
   name: string;
-  billboardLabel: string;
+  value: string;
   createdAt: string;
 };
 
-const columns: ColumnDef<CategoryColumn>[] = [
+const columns: ColumnDef<SizeColumn>[] = [
   {
     accessorKey: 'name',
     header: 'Name',
   },
   {
-    accessorKey: 'billboard',
-    header: 'Billboard',
-    cell: ({ row }) => row.original.billboardLabel,
+    accessorKey: 'value',
+    header: 'Value',
   },
   {
     accessorKey: 'createdAt',
