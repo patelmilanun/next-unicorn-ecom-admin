f957bd30694545ef80957d7b390635768542f637
Milan Patel <patelmilanun@gmail.com>
2023-07-19 11:49:10 +0530

    feat: Add dashboard module - Add charts using recharts - Add cards for sale, revenue and stock

---
+++ src/components/overview.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
+
+interface OverviewProps {
+  data: any[];
+}
+
+export default async function Overview(props: OverviewProps) {
+  const data = props?.data;
+  return (
+    <ResponsiveContainer width="100%" height={350}>
+      <BarChart data={data}>
+        <XAxis
+          dataKey="name"
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+        />
+        <YAxis
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+          tickFormatter={(value) => `$${value}`}
+        />
+        <Bar dataKey="total" fill="#3498db" radius={[4, 4, 0, 0]} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+}
